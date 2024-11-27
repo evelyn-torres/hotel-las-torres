ab7fc0c77ecaced554d85391c0a8bac6ef8b25c6
ayushigup4 <125705383+ayushigup4@users.noreply.github.com>
2024-11-26 20:37:12 -0500

    made some edits to booking form and also deleted static folder

--- public/js/contactForm.js
+++ public/js/contactForm.js
@@ -1,7 +1,6 @@
 let contactForm = document.getElementById('feedback-form');
 
 contactForm.addEventListener('submit', (event) => {
-
     event.preventDefault();
     console.log("Form Submission Fired")
 })
