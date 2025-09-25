ef3da75e5360fe07f88568594b7791664d26663e
Evelyn Torrres <etorres.et905@gmail.com>
2025-09-25 13:39:30 -0500

    added something to cookie secure

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(
       ttl: 14 * 24 * 60 * 60, // 14 days
     }),
     cookie: {
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === 'production'&& process.env.VERCEL_ENV === 'production',
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 2,
       sameSite: 'lax',
